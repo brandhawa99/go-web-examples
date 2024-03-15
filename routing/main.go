5fad3203ddadca2556eb1c598f4c007383154408
brandhawa99 <baltej.randhawa0@gmail.com>
2024-03-15 09:38:46 -0700

    added sub routes

--- routing/main.go
+++ routing/main.go
@@ -17,5 +17,18 @@ func main() {
 
 		fmt.Fprintf(w, "You've requested the book: %s on page %s\n", title, page)
 	})
+
+	r.HandleFunc("/books/{title}", CreateBook).Methods("POST") // restricts request handler to specific HTTP methods
+	r.HandleFunc("/books/{title}", ReadBook).Methods("GET")
+	r.HandleFunc("/books/{title}", UpdateBook).Methods("PUT")
+	r.HandleFunc("/books/{title}", DeleteBook).Methods("DELETE")
+
+	r.HandleFunc("/books/{title}", BookHandler).Host("www.mybookstore.com") // restrict the request handler to specific hostnames or subdomains
+
+	bookrouter := r.PathPrefix("/books").Subrouter() // resticts the request handler to specific path prefixes
+	bookrouter.HandleFunc("/", AllBooks)
+	bookrouter.HandleFunc("/{title}", GetBook)
+
 	http.ListenAndServe(":8080", r)
+
 }
