3e844383949c0113c9dc311ad7820e128cb86f63
brandhawa99 <baltej.randhawa0@gmail.com>
2024-03-15 00:32:04 -0700

    created server with gorilla mux

---
+++ routing/go.mod
@@ -0,0 +1,5 @@
+module github.com/brandhawa/go-web-examples/routing
+
+go 1.22.0
+
+require github.com/gorilla/mux v1.8.1
---
+++ routing/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	r := mux.NewRouter()
+
+	r.HandleFunc("/books/{title}/page/{page}", func(w http.ResponseWriter, r *http.Request) {
+		vars := mux.Vars(r)
+		title := vars["title"] // book title slug
+		page := vars["page"]   // the page
+
+		fmt.Fprintf(w, "You've requested the book: %s on page %s\n", title, page)
+	})
+	http.ListenAndServe(":8080", r)
+}
