05e2316288f9050bb3c2baa135d6aa8e39c8749d
brandhawa99 <baltej.randhawa0@gmail.com>
2024-03-18 15:10:45 -0700

    postgres database

--- mysql-database/go.mod
+++ mysql-database/go.mod
@@ -4,8 +4,4 @@ go 1.22.0
 
 require github.com/lib/pq v1.10.9
 
-require (
-	filippo.io/edwards25519 v1.1.0 // indirect
-	github.com/go-sql-driver/mysql v1.8.0 // indirect
-	github.com/joho/godotenv v1.5.1 // indirect
-)
+require github.com/joho/godotenv v1.5.1
--- mysql-database/main.go
+++ mysql-database/main.go
@@ -14,66 +14,101 @@ import (
 func goDotEnvVariable(key string) string {
 	err := godotenv.Load(".env")
 	if err != nil {
-		log.Fatal("Error loading .env file")
+		log.Fatalf("Error loading .env file")
 	}
 	return os.Getenv(key)
 }
-func main() {
 
-	godotenv.Load()
-	userName := goDotEnvVariable("PGUSER")
-	pass := goDotEnvVariable("PGPASSWORD")
-	connStr := fmt.Sprintf("postgresql://%s:%s@ep-damp-frost-a61hgvof.us-west-2.aws.neon.tech/test-mysql?sslmode=require", userName, pass)
+func main() {
+	pgusername := goDotEnvVariable("PGUSER")
+	pgpassword := goDotEnvVariable("PGPASSWORD")
+
+	connStr := fmt.Sprintf("postgresql://%s:%s@ep-long-hill-a64guqb7.us-west-2.aws.neon.tech/test?sslmode=require", pgusername, pgpassword)
 	db, err := sql.Open("postgres", connStr)
 	if err != nil {
 		fmt.Println(err.Error())
 	}
-	defer db.Close()
 
-	rows, err := db.Query("select version()")
-	if err != nil {
-		log.Fatal(err)
+	// query := `
+	// 	CREATE TABLE users (
+	// 	  id SERIAL,
+	//  	 	username TEXT NOT NULL,
+	//   	password TEXT NOT NULL,
+	//   	created_at TIMESTAMP,
+	//   	PRIMARY KEY (id)
+	// );`
+	// _, err = db.Exec(query)
+	// if err != nil {
+	// 	fmt.Println(err.Error())
+	// }
+	{
+		//inserting a new row
+		username := "johndoe"
+		password := "secret"
+		createdAt := time.Now()
+
+		_, err := db.Exec(`INSERT INTO users (username, password, created_at) VALUES ($1, $2, $3)`, username, password, createdAt)
+		if err != nil {
+			fmt.Println(err.Error())
+		}
+
+		if err != nil {
+			fmt.Println(err.Error())
+		}
+
 	}
-	defer rows.Close()
 
-	var version string
-	for rows.Next() {
-		err := rows.Scan(&version)
+	{ // Query a single user
+		var (
+			id        int
+			username  string
+			password  string
+			createdAt time.Time
+		)
+
+		query := "SELECT id, username, password, created_at FROM users WHERE id = $1"
+		if err := db.QueryRow(query, 1).Scan(&id, &username, &password, &createdAt); err != nil {
+			log.Fatal(err)
+		}
+
+		fmt.Println(id, username, password, createdAt)
+	}
+
+	{ // Query all users
+		type user struct {
+			id        int
+			username  string
+			password  string
+			createdAt time.Time
+		}
+
+		rows, err := db.Query(`SELECT id, username, password, created_at FROM users`)
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer rows.Close()
+
+		var users []user
+		for rows.Next() {
+			var u user
+
+			err := rows.Scan(&u.id, &u.username, &u.password, &u.createdAt)
+			if err != nil {
+				log.Fatal(err)
+			}
+			users = append(users, u)
+		}
+		if err := rows.Err(); err != nil {
+			log.Fatal(err)
+		}
+
+		fmt.Printf("%#v", users)
+	}
+
+	{
+		_, err := db.Exec(`DELETE FROM users WHERE id = ?`, 1)
 		if err != nil {
 			log.Fatal(err)
 		}
 	}
-	fmt.Printf("version=%s\n", version)
-	if err != nil {
-		fmt.Println(err.Error())
-	}
-	query := `
-		CREATE TABLE users(
-			id INT AUTO_INCREMENT,
-			username TEXT NOT NULL,
-			password TEXT NOT NULL,
-			created_at DATETIME,
-			PRIMARY KEY (id)
-		);`
-
-	_, err = db.Exec(query)
-	if err != nil {
-		fmt.Println(err.Error())
-	}
-
-	username := "johndoe"
-	password := "secret"
-	createAt := time.Now()
-
-	result, err := db.Exec(`INSERT INTO users(username, password, create_at) VALUES (?,?,?)`, username, password, createAt)
-	if err != nil {
-		fmt.Println(err.Error())
-	}
-
-	userId, err := result.LastInsertId()
-	if err != nil {
-		fmt.Println(err.Error())
-	}
-
-	fmt.Println(userId)
 }
